76b56b79caedf1d671d974564fbed3c78981bf92
Dominic Wroblewski <dominic.wroblewski@sky.uk>
2020-08-07 14:52:58 +0100

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import CloudspongeVue from '@/cloudsponge-vue.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    CloudspongeVue
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <cloudsponge-vue />
+  </div>
+</template>
---
+++ src/cloudsponge-vue.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'CloudspongeVue', // vue component name
+  props: {
+    apiKey: {
+      type: String,
+      default: '6PCap7zRaiSNYcpB4znxpQ',
+    },
+  },
+  mounted() {
+    let externalScript = document.createElement('script');
+    externalScript.setAttribute('src', `https://api.cloudsponge.com/widget/${this.apiKey}.js`);
+    document.head.appendChild(externalScript);
+  },
+});
+</script>
+
+<template>
+  <div class="cloudsponge-vue">
+    <div class="cloudsponge-launch">
+      <span>Something</span>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
